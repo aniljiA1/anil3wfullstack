e1a19f409bb14e950928adbc5d48b0926c66ccc5
Anil Kumar <126413189+aniljiA1@users.noreply.github.com>
2026-04-02 15:26:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,23 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://anil3wfullstack.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    },
+    credentials: true,
+  }),
+);
 app.use(express.json({ limit: "10mb" }));
 
 app.get("/", (req, res) => {
